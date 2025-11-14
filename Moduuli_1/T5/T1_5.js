b15f67e526a3cf160c101cbd204833ff31afc4e7
ViliJ94 <vili.johansson@hotmail.fi>
2025-11-14 10:58:37 +0200

    Koulutehtävät - korjauksia

--- Moduuli_1/T5/T1_5.js
+++ Moduuli_1/T5/T1_5.js
@@ -2,14 +2,7 @@
 let year = prompt('Enter the year');
 year = parseInt(year);
 
-let isLeap = false
 if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-  isLeap = true
-} else {
-  isLeap = false
-}
-if (isLeap)
-{
   document.querySelector('#leaping').innerHTML = `${year} is a leap year.`;
 } else {
   document.querySelector('#leaping').innerHTML = `${year} isn't a leap year.`;
