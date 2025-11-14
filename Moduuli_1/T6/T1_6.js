5ec93ffccda3d752d3caca07bd47823e1d226ec7
ViliJ94 <vili.johansson@hotmail.fi>
2025-11-14 11:08:32 +0200

    Koulutehtävät - korjauksia

--- Moduuli_1/T6/T1_6.js
+++ Moduuli_1/T6/T1_6.js
@@ -4,10 +4,12 @@ let confirmation = confirm('Should I calculate the square root?');
 
 if (confirmation) {
   let number = parseFloat(prompt('Enter a number'));
-  let square_root = Math.sqrt(number);
-  document.querySelector('#sqrt').innerHTML = `The square root of ${number} is ${square_root};`;
-
-} else {
+   if (number < 0) {
+      document.querySelector('#sqrt').innerHTML = 'The square root of a negative number is not defined.';}
+   else {
+     let square_root = Math.sqrt(number);
+     document.querySelector('#sqrt').innerHTML = `The square root of ${number} is ${square_root};`;
+}}
+else {
   document.querySelector('#sqrt').innerHTML = 'The square root is not calculated.';
 }
-
